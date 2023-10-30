6ec851b33d15c9fc2af2b706c3c8158ae9316cfa
Chinchill <91843144+Chinchillus@users.noreply.github.com>
2023-10-30 13:32:43 +0100

    Update and rename CSC-Lite v1.1.bat to CSC-Lite.bat

--- CSC-Lite.bat
+++ CSC-Lite.bat
@@ -1,10 +1,32 @@
+rem MIT License
+
+rem Copyright (c) 2023 Chinchill
+
+rem Permission is hereby granted, free of charge, to any person obtaining a copy
+rem of this software and associated documentation files (the "Software"), to deal
+rem in the Software without restriction, including without limitation the rights
+rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem copies of the Software, and to permit persons to whom the Software is
+rem furnished to do so, subject to the following conditions:
+
+rem The above copyright notice and this permission notice shall be included in all
+rem copies or substantial portions of the Software.
+
+rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+rem SOFTWARE.
+
 cls
 @echo off
 color c
 MODE 140,40
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%Temp%\getadmin.vbs" del "%Temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul  || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%Temp%\getadmin.vbs" && "%Temp%\getadmin.vbs" && exit /B )
-title CSC Lite (v1.1 "VNice")
+title CSC Lite (v1.1 "bruh")
 cd /
 :start
 echo.
@@ -25,7 +47,7 @@ echo    CC:::::::::::::::C S::::::SSSSSS:::::S    CC:::::::::::::::C L::::::::::
 echo      CCC::::::::::::C S:::::::::::::::SS       CCC::::::::::::C L::::::::::::::::::::::L
 echo         CCCCCCCCCCCCC  SSSSSSSSSSSSSSS            CCCCCCCCCCCCC LLLLLLLLLLLLLLLLLLLLLLLL
 echo.
-echo Witaj, %USERNAME%!
+echo Welcome, %USERNAME%!
 echo.
 timeout /t 1 /nobreak >nul
 goto csc
@@ -57,4 +79,3 @@ del /s /f /q %LocalAppData%\CrashDumps\*.* 2>nul
 
 taskkill /F /IM explorer.exe & start explorer
 exit
-rem if u are stealing my code please just add me to the credit @chinchill (Discord)
